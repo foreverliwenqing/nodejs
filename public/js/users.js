24c8f8d92f08253a7fdd46496c87f80919319dab
foreverliwenqing <1243429975@qq.com>
2018-11-29 21:02:05 +0800

    登录注册完整页面

--- public/js/users.js
+++ public/js/users.js
@@ -1,19 +1,3 @@
-//  <tr>
-//     <th scope="row">1</th>
-//     <td>Mark</td>
-//     <td>Otto</td>
-//     <td>@mdo</td>
-// </tr>
-
-var tbody = document.querySelector('tbody');
-
-
-$.getJSON('/person.json',function(res){
-
-    console.log(res);
-    console.log(1);
-})
-
 
 
 
